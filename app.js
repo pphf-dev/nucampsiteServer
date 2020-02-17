c2a24bfeb6d021db37bee3b1cde9eb956fb11391
Paula Frizzell <pphf@use.startmail.com>
2020-02-17 15:43:23 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -41,6 +41,34 @@ app.use(express.json());
 // output => JSON Object
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); //parser for cookies
+
+//Authentication - custom middleware function named auth()
+function auth(req, res, next) {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        const err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic'); //lets client know the server is requesting authentication and the authentication method requested is 'Basic'.
+        err.status = 401;
+        return next(err);
+    }
+    //parse authorization header and validate username and password
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user ==='admin' && pass === 'password') {
+      return next(); //authorized
+    } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+}
+
+app.use(auth);
+
 //Directory for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
